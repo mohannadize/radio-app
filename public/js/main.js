c87550ed36357a1deca5bcbabb51dc056663c115
Mohannad Hisham <Mohannadize15@gmail.com>
2020-05-09 20:01:41 +0200

    sw enabled

--- public/js/main.js
+++ public/js/main.js
@@ -112,4 +112,4 @@ function player(options) {
 }
 populate_main();
 
-// if ("serviceWorker" in navigator) navigator.serviceWorker.register("sw.js");
+if ("serviceWorker" in navigator) navigator.serviceWorker.register("sw.js");
